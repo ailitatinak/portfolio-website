130d47591bb6722ef976746d370c4269d02bc83e
ailitatinak <kastanjekofta@gmail.com>
2024-11-21 15:50:00 +0100

    root styling, gradient ellipse components

---
+++ src/components/GradientEllipses.jsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+function BlueEllipse() {
+  return (
+    <>
+      <div className="div" />
+      <style jsx>{`
+        .div {
+          display: flex;
+          min-height: 0px;
+          max-width: 0px;
+        }
+      `}</style>
+    </>
+  );
+}
+
+function LimeEllipse() {
+  return (
+    <>
+      <div className="div" />
+      <style jsx>{`
+        .div {
+          display: flex;
+          min-height: 0px;
+          max-width: 0px;
+        }
+      `}</style>
+    </>
+  );
+}
+
+function TaupeEllipse() {
+  return (
+    <>
+      <div className="div" />
+      <style jsx>{`
+        .div {
+          display: flex;
+          min-height: 0px;
+          max-width: 0px;
+        }
+      `}</style>
+    </>
+  );
+}
