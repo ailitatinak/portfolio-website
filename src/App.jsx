16ae8a3e82858d3bb5162dc9f420ecd041d56b33
ailitatinak <kastanjekofta@gmail.com>
2024-11-26 16:00:46 +0100

    basic home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/Home'
 import { AboutPage } from './pages/About'
 import { PortfolioPage } from './pages/Portfolio'
+import './styles/home.css'
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 export function HomePage() {
     return (
-      <>
-        <h1>This is the home page</h1>
-      </>
+        <>
+            <div className="pageContainer">
+                <h1 className="nameHeader">Tilia Larson Tioukalias</h1>
+                {/* <img className="faceImg" src="/src/assets/face.png" alt="face" /> */}
+                <img className="scribble1" src="/src/assets/bwscribble1.png" alt="scribble-1" />
+                <img className="scribble2" src="/src/assets/bwscribble1.png" alt="scribble-1" />
+                <img className="scribble3" src="/src/assets/bwscribble3.png" alt="scribble-3" />
+                <img className="scribble4" src="/src/assets/bwscribble4.png" alt="scribble-4" />
+                <nav className="navBar">
+                    <p>About me</p>
+                    <p>Portfolio</p>
+                </nav>
+            </div>
+        </>
     )
-  }
+}
