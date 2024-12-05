8f3c397f19f04ce8146eb33301a6233b4310697c
ailitatinak <kastanjekofta@gmail.com>
2024-12-05 13:45:32 +0100

    Art: About - grund färdig

--- src/components/AAboutContent.jsx
+++ src/components/AAboutContent.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import '../styles/a-about.css'
 
 export function AAboutContent() {
     return (
       <>
-        <h1>This is the about content</h1>
+        <div className='aboutContent'>
+          <div className='imgPlaceholder'></div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsa accusantium minima ipsam? <br /><br />Molestias facilis ratione excepturi quisquam, quasi nesciunt qui voluptatibus, quod aliquam architecto in. Similique odio a possimus.<br />
+          <br />Temporibus at asperiores eligendi quo id, sapiente odio iure consequuntur necessitatibus consectetur corporis, nesciunt omnis eius corrupti. <br />Impedit, doloribus repudiandae cum veniam exercitationem!</p>
+        </div>
       </>
     )
   }
--- src/pages/AAbout.jsx
+++ src/pages/AAbout.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import { ANavBar } from '../components/ArtNavBar'
 import '../styles/a-about.css'
+import { AAboutContent } from "../components/AAboutContent"
 
 export function AAboutPage() {
   return (
     <>
       <ANavBar />
-      <h1>This is the art about page</h1>
+      <body className="aboutBody">
+        <AAboutContent />
+      </body>
     </>
   )
 }
